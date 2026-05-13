573d484c2a6eab0d500ab251f1c271bbf810e371
Ladislav Zezula <zezula@volny.cz>
2026-05-13 08:44:34 +0200

    Added detections of newer versions of InnoSetup (#141)

--- tools/fileinfo/detection/installers/inno/test.py
+++ tools/fileinfo/detection/installers/inno/test.py
@@ -1,12 +1,41 @@
 from regression_tests import *
 
-class TestInnoSetupDetection(Test):
+class TestInnoSetupDetection_540(Test):
     settings = TestSettings(
         tool='fileinfo',
-        input='inno.exe'
+        input='inno-5.4.0.exe_'
     )
 
     def test_detected_inno(self):
         assert self.fileinfo.succeeded
         assert self.fileinfo.output.contains(r'.*Inno Setup \(5.4.0 - 5.5.1\)')
 
+class TestInnoSetupDetection_670(Test):
+    settings = TestSettings(
+        tool='fileinfo',
+        input='inno-6.7.0.exe_'
+    )
+
+    def test_detected_inno(self):
+        assert self.fileinfo.succeeded
+        assert self.fileinfo.output.contains(r'.*Inno Setup \(6.x\)')
+
+class TestInnoSetupDetection_700(Test):
+    settings = TestSettings(
+        tool='fileinfo',
+        input='inno-7.0.0.exe_'
+    )
+
+    def test_detected_inno(self):
+        assert self.fileinfo.succeeded
+        assert self.fileinfo.output.contains(r'.*Inno Setup \(7.x\)')
+
+class TestInnoUnistallerDetection(Test):
+    settings = TestSettings(
+        tool='fileinfo',
+        input='inno-uninstall.exe_'
+    )
+
+    def test_detected_inno(self):
+        assert self.fileinfo.succeeded
+        assert self.fileinfo.output.contains(r'.*Inno Uninstaller')
